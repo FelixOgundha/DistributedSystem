6e4382b7c4aa0830facd290461df4a4fd59f5627
fogundha <fogundha@strathmore.edu>
2025-04-02 16:57:37 +0300

    updated event processing

---
+++ Services/CommandsService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,37 @@
+﻿using CommandsService.Data;
+using MapsterMapper;
+
+namespace CommandsService.EventProcessing
+{
+    public class EventProcessor : IEventProcessor
+    {
+        private readonly AppDbContext _context;
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMapper _mapper;
+
+        public EventProcessor(
+            AppDbContext context,
+            IServiceScopeFactory scopeFactory,
+            IMapper mapper)
+        {
+            _context = context;
+            _scopeFactory = scopeFactory;
+            _mapper = mapper;
+        }
+        public void ProcessEvent(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+
+    enum EventType
+    {
+        PlatformPublished,
+        Undetermined
+    }
+
+
+}
